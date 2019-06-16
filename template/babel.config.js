10cb749c9c380a699eeb2efdebcb8a8ca9908463
babie <babie.wildheart@gmail.com>
2019-06-16 19:51:50 +0900

    fix: Make eslint be silence on `babel.config.js`

--- template/babel.config.js
+++ template/babel.config.js
@@ -1,20 +1,27 @@
+// eslint-disable-next-line @typescript-eslint/no-var-requires
 const poiBabelConfig = require('poi/babel')
 
-const { presets, plugins } = poiBabelConfig(null, { 'jsx': 'react', 'typescript': true })
+const { presets, plugins } = poiBabelConfig(null, {
+  jsx: 'react',
+  typescript: true
+})
 module.exports = {
-  "presets": presets,
-  "plugins": plugins.concat([[
-    "module-resolver", {
-      "root": ["./src"],
-      "alias": {
-        "react": "preact/compat",
-        "react-dom": "preact/compat"
+  presets: presets,
+  plugins: plugins.concat([
+    [
+      'module-resolver',
+      {
+        root: ['./src'],
+        alias: {
+          react: 'preact/compat',
+          'react-dom': 'preact/compat'
+        }
       }
-    }
-  ]]),
-  "env": {
-    "test": {
-      "presets": presets
+    ]
+  ]),
+  env: {
+    test: {
+      presets: presets
     }
   }
 }
